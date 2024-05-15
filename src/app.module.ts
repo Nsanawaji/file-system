4f009d88fab8ed574e8ff92bb6327070f20e6a57
Nsanawaji <nsanawajiadasi@gmail.com>
2024-05-15 11:18:07 +0100

    Adding config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MulterModule } from '@nestjs/platform-express';
 import { FileModule } from './file/file.module';
-import {ThrottlerModule} from '@nestjs/Throttler';
+import { ThrottlerModule } from '@nestjs/Throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard } from '@nestjs/Throttler';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000,
-        limit: 10,
-      },
-    ]),
+    ThrottlerModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => [
+        {
+          ttl: configService.getOrThrow('THROTTLER_TTL'),
+          limit: configService.getOrThrow('THROTTLER_LIMIT'),
+        },
+      ],
+      inject: [ConfigService],
+    }),
     FileModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
